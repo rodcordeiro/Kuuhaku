b7544ff63fe1ab0e905d8b7b78ab9ae34a32f0c5
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-03-09 13:56:50 -0300

    Removed unused folder

--- app/src/models/channel.py
+++
@@ -1,8 +0,0 @@
-class Channel:
-    def __init__(self,channel_id,channel_name,nsfw,news,category_id):
-        self.channel_id = channel_id
-        self.channel_name = channel_name
-        self.nsfw = nsfw
-        self.news = news
-        self.category_id = category_id
-        
--- app/src/models/user.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self, author_id, author_name, author_nick, isBot):
-        self.author_id = author_id
-        self.author_name = author_name
-        self.author_nick = author_nick
-        self.isBot = isBot
